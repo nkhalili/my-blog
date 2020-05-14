3ca0bd99bc858bdbecf5aa6d66255387c40973f3
Navid <nkhalili@live.com>
2020-05-13 21:18:14 -0400

    Added HomePage + router

---
+++ src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit rem, sed
+      error cupiditate explicabo nisi. Aut minima assumenda, delectus commodi
+      iste, et sint blanditiis repellendus minus voluptatibus quidem ipsa
+      dolores. Quis architecto vitae culpa doloremque sapiente hic expedita
+      fugit quibusdam tempora molestias soluta delectus cumque ab harum corporis
+      similique exercitationem, repellat voluptatibus repellendus, dolor
+      corrupti consectetur natus officia. Dolor, accusamus. Sapiente obcaecati
+      recusandae veniam, corporis iste porro eaque vitae dicta ipsum? Quasi
+      assumenda ad expedita libero reprehenderit esse deleniti quis architecto,
+      ut illo. Architecto molestiae laborum mollitia asperiores soluta minima.
+      Hic, officia beatae ipsum illum perspiciatis accusantium quo vel cumque
+      tempore fuga quisquam pariatur ea recusandae unde consequatur soluta
+      aliquid laudantium adipisci blanditiis nisi eaque dolorem provident
+      reiciendis temporibus. Sit!
+    </p>
+  </>
+);
+
+export default HomePage;
