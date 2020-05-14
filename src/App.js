531fa318e574c4e8458f45d5dd6526a054e0abe9
Navid <nkhalili@live.com>
2020-05-13 21:31:32 -0400

    Added pages + routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import HomePage from './pages/HomePage';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
