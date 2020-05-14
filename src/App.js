c4a3084f454f5f5a3aa9f093f016f25d272a2d88
Navid <nkhalili@live.com>
2020-05-13 21:21:44 -0400

    Using router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
   );
 }
 
