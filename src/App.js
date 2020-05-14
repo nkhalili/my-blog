adad50806fab2713b2ddb243e1bffa2f6298dcb6
Navid <nkhalili@live.com>
2020-05-13 22:27:54 -0400

    Adding Navigation bar

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
