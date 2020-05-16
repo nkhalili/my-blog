f0e5b60e1a44b25dfb963341698e710e13888353
Navid <nkhalili@live.com>
2020-05-16 13:50:09 -0400

    Fixed articles links

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -7,7 +7,7 @@ const ArticlesList = ({ articles }) => (
       <Link
         className="article-list-item"
         key={key}
-        to={`article/${article.name}`}
+        to={`/article/${article.name}`}
       >
         <h3>{article.title}</h3>
         <p>{article.content[0].substring(0, 150)}...</p>
